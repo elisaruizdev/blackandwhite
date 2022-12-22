3a70b2c8d20f9498f71b0822793773c1a3f2b4e0
ruizortega22 <ruiz.ortega@bootcamp-upgrade.com>
2022-12-22 21:30:13 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
+import { NavegadorComponent } from './shared/layout/navegador/navegador.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    NavegadorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
